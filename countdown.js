a0eb61c51de8d9974f5f783d9320f08c7bc415dc
Jeffg2221 <109566277+Jeffg2221@users.noreply.github.com>
2022-11-21 12:45:17 -0500

    second commit

--- countdown.js
+++ countdown.js
@@ -3,3 +3,12 @@ console.log(' testing one two....hello world');
 // A function is a set of instructions that we can execute and we can pass in data to it as inputs
 
 // Create a function that will accept a number as an input to count down  from , and it will count down that number until it hits 0;
+function countDown(num, stopNum){ //num is the parameter
+    // this is the bodyof the function
+    for( let i = num; i >= stopNum; i--){
+        console.log(i)
+    }
+    console.log( "finished")
+
+
+}
