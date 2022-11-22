38e4fc3b5ca0dc9944b229b0ab069d1360a6cb53
Jeffg2221 <109566277+Jeffg2221@users.noreply.github.com>
2022-11-21 21:43:36 -0500

    second commits

---
+++ array.js
@@ -0,0 +1,19 @@
+// in a for loop, inside the parenthrsis, there atr trhree parts:
+// let i = 5--> where to start our loop from
+// middle part ( i>=1) --> when to stop looping the process
+// third part ( i--) --> what to do with our variable after each iteration 
+let nbaGoats = ["Lebron", "Kobe", "Jordan", "Shaq"];
+function printArr(arr) {
+    for (let i = 0; i <= 4; i++) {
+        console.log(nbaGoats[i])
+    }
+}
+printArr(nbaGoats)
+
+let Nums = [15, 22, 33, 77, 99, 88, 55];
+function PrintgreaterthanValue(arr) {
+    let secondVal = arr[i]
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr(i));
+    }
+}
---
+++ greaterThan.js
@@ -0,0 +1,16 @@
+
+for( let i = 1; i <= 255; i=i+2){
+    console.log(i)
+}
+
+
+let Numtwo = [ 15, 22, 33, 77, 99, 88, 55];
+function PrintgreaterthanValue(arr){
+    let secondVal = arr[i]
+    for( let i = 0; i < arr.length; i++ ){
+        if( arr[i] > secondVal){
+            console.log(arr(i)); 
+        }
+    }
+}
+PrintgreaterthanValue(arr[i])
