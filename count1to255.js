faff95b3e5d4b1674b0727632a10c519495e16d6
Jeffg2221 <109566277+Jeffg2221@users.noreply.github.com>
2022-11-22 12:13:23 -0500

    count 1 to 255

---
+++ count1to255.js
@@ -0,0 +1,15 @@
+//Write a function that returns an array with all the numbers from 1 to 255. You may use the push() function for this exercise.
+
+
+
+function get_array(){
+    var arr = [];
+    for(let i = 1; i<= 255; i++){
+        arr.push(i);
+    }
+    return arr;
+
+}
+console.log(get_array())
+
+
