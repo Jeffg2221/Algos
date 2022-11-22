89bc6ba046276b2d1a81285cae1fc2da4c2e9117
Jeffg2221 <109566277+Jeffg2221@users.noreply.github.com>
2022-11-22 12:45:33 -0500

    sum even numbers

---
+++ sum_even_numbers.js
@@ -0,0 +1,15 @@
+
+//Write a function that would get the sum of all the even numbers from 1 to 1000. You may use a modulus operator for this exercise.
+
+function sum_even_numbers(){
+    let sum = 0;
+    for(let i = 0; i <= 1000; i++ ){
+        if(i % 2 === 0) {
+            sum += i;
+        }
+    
+    }
+return sum;
+}
+
+console.log(sum_even_numbers())
