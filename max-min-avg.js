3f255f20f1f8fae26a3204832057d70fc00d25e2
Jeffg2221 <109566277+Jeffg2221@users.noreply.github.com>
2022-12-05 22:04:14 -0500

    final commit

--- max-min-avg.js
+++ max-min-avg.js
@@ -5,13 +5,13 @@ const arr1 = [1, 5, 10, -2]
 
 function maxMinAvg(arr) {
 
-    let max = arr[0];
-    let min = arr[0];
-    let sum = arr[0];
+    let max = arr[0]; // equal to first value of array
+    let min = arr[0];// equal to first value of array
+    let sum = arr[0];// equal to first value of array
 
-    for (let i = 1; i < arr.length; i++) {
+    for (let i = 1; i < arr.length; i++) { // for loop to loop through length of array
         if (arr[i] > max) {
-            max = arr[i];
+            max = arr[i]; // if array of i is greater than current max set max to current value
         }
         if (arr[i] < min) {
             min = arr[i];
